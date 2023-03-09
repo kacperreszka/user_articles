75fc0d3dd5654c547857451c833626fa4962c349
Kacper Reszka <kapiresz@gmail.com>
2023-03-09 19:53:16 +0100

    Add RemoteDataBase with Dio

--- lib/data/remote_data_sources/authors_remote_data_source.dart
+++ lib/data/remote_data_sources/authors_remote_data_source.dart
@@ -1,3 +1,5 @@
+import 'package:dio/dio.dart';
+
 class AuthorsMockedDataSource {
   Future<List<Map<String, dynamic>>?> getAuthors() async {
     return [
@@ -15,4 +17,16 @@ class AuthorsMockedDataSource {
       },
     ];
   }
+}
+
+class AuthorRemoteDioDataSource { //Pobieranie danych z Dio
+   Future<List<Map<String, dynamic>>?> getAuthors() async {
+    final response = await Dio().get<List<dynamic>>('https://my-json-server.typicode.com/adamsmaka/json-demo/users');
+    final listDynamic = response.data;
+    if (listDynamic == null) {
+      return null;
+    }
+    return listDynamic.map((e) => e as Map<String, dynamic>).toList();
+   }
+
 }
--- lib/domain/models/article_model.dart
+++ lib/domain/models/article_model.dart
@@ -1,16 +1,24 @@
+import 'package:json_annotation/json_annotation.dart';
+
+part 'article_model.g.dart';
+
+@JsonSerializable()
 class ArticleModel {
   const ArticleModel({
     required this.id,
     required this.authorId,
     required this.content,
   });
-
+  
   final int id;
-  final int authorId;
+
+  @JsonKey(name: 'author_id')
+  final int? authorId;
+
   final String content;
 
-  ArticleModel.fromJson(Map<String, dynamic> json)
-      : id = json['id'],
-        authorId = json['author_id'],
-        content = json['content'];
+  factory ArticleModel.fromJson(Map<String, dynamic> json) => _$ArticleModelFromJson(json);
+
+  Map<String, dynamic> toJson() => _$ArticleModelToJson(this);
+
 }
